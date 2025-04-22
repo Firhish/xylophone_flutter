7af9fd6cf824d57ad7700305411be6162b763941
Firhish <86069053+Firhish@users.noreply.github.com>
2025-04-22 23:24:47 +0800

    progress1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +9,66 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              TextButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note1.wav'));
+                  print('clicked');
+                },
+                child: Container(color: Colors.red, height: 100),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note2.wav'));
+                  print('clicked');
+                },
+                child: Container(color: Colors.orange, height: 100),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note3.wav'));
+                  print('clicked');
+                },
+                child: Container(color: Colors.yellow, height: 100),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note4.wav'));
+                  print('clicked');
+                },
+                child: Container(color: Colors.lightGreen, height: 100),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note5.wav'));
+                  print('clicked');
+                },
+                child: Container(color: Colors.green, height: 100),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note6.wav'));
+                  print('clicked');
+                },
+                child: Container(color: Colors.blue, height: 100),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note7.wav'));
+                  print('clicked');
+                },
+                child: Container(color: Colors.purple, height: 100),
+              ),
+            ],
+          ),
         ),
       ),
     );
